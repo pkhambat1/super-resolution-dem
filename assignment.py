af8548087567213d24e377366bf9bdc0bde7159c
Nimisha Wagle <wagle1996@gmail.com>
2021-11-25 18:23:42 -0500

    changed_loss_func

--- assignment.py
+++ assignment.py
@@ -165,20 +165,15 @@ class Model(tf.keras.Model):
 
 
 
-    def tf_psnr(mse):
+    def tf_psnr(ori_high_res,pred_high_res):
         """
         PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
 
-        It can be calculated as
-        PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
+        Adopted from "https://www.tensorflow.org/api_docs/python/tf/image/psnr"
 
-        When providing an unscaled input, MAXp = 255. Therefore 20 * log10(255)== 48.1308036087.
-        However, since we are scaling our input, MAXp = 1. Therefore 20 * log10(1) = 0.
-        Thus we remove that component completely and only compute the remaining MSE component.
-
-        Modify from https://github.com/titu1994/Image-Super-Resolution
         """
-        return -10. * tf.log(mse) / tf.log(10.)
+        psnr=tf.image.psnr(ori_high_res, pred_high_res, max_val=255, name=None)
+        return psnr
 
     
     def accuracy(self, logits, labels):
